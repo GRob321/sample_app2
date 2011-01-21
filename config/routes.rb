72ed6f590d4e61910d99cec0bb6ffe7462bc14af
Greg Robinson <robinson321@gmail.com>
2011-01-21 02:42:56 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 SampleApp2::Application.routes.draw do
  
+  get "users/new"
+
+  get "user/new"
+
+  match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
